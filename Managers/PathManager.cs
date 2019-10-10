826ca53813e1e83887f20b74b10f5e6395d19ed2
Seth Nielsen <seth.emanuel.nielsen@gmail.com>
2019-10-10 07:06:10 -0700

    Add project files.

---
+++ Managers/PathManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FairytaleStoryGenerator.Managers
+{
+    class PathManager
+    {
+    }
+}
---
+++ Model/NodeStory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FairytaleStoryGenerator
+{
+    class NodeStory
+    {
+        public string ID;
+        public string text;
+        public Dictionary<string, string> storyGlobals;
+
+        public string[] continuations;
+    }
+}
